435c95a2c1e8da075bdcc8604fa59075d540115a
Veronica Trias <98854511+VeroRamm@users.noreply.github.com>
2022-08-08 19:32:01 -0300

    Encriptador de texto

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+/*`La letra "e" es convertida para "enter"`
+`La letra "i" es convertida para "imes"`
+`La letra "o" es convertida para "ober"`
+`La letra "a" es convertida para "ai"`
+`La letra "u" es convertida para "ufat"`*/
+
+
+let txtEncriptado
+function encriptar(){
+   const texto = document.getElementById("areaTexto").value;
+   txtEncriptado = texto.replace(/e/img, "enter" );
+   txtEncriptado = txtEncriptado.replace(/i/img, "imes" );
+   txtEncriptado = txtEncriptado.replace(/o/img, "ober" );
+   txtEncriptado = txtEncriptado.replace(/a/img, "ai" );
+   txtEncriptado = txtEncriptado.replace(/u/img, "ufat" );
+
+      document.getElementById("default").style.display = "none";
+      document.getElementById("visualizador").style.display = "flex";
+      document.getElementById("lectura").innerHTML = txtEncriptado;
+      document.getElementById("areaTexto").value = "";
+}
+function desencriptar(){
+   const texto = document.getElementById("areaTexto").value;
+   txtEncriptado = texto.replace(/enter/img, "e" );
+   txtEncriptado = txtEncriptado.replace(/imes/img, "i" );
+   txtEncriptado = txtEncriptado.replace(/ober/img, "o" );
+   txtEncriptado = txtEncriptado.replace(/ai/img, "a" );
+   txtEncriptado = txtEncriptado.replace(/ufat/img, "u" );
+   
+      document.getElementById("default").style.display = "none";
+      document.getElementById("visualizador").style.display = "flex";
+      document.getElementById("lectura").innerHTML = txtEncriptado;
+   
+}
+function copiar(){
+ let txtCopiar = document.querySelector("#lectura");
+ txtCopiar.select();
+ document.execCommand("copy");
+}
